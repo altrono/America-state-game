5bcd858514808d85f8893a1c9a95a535418b446b
altrono <hbotema16@gmail.com>
2023-01-28 00:25:09 +0200

    Added Main logic of the game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title('U.S. States Game')
 image = "states.gif"
@@ -11,10 +11,20 @@ turtle.shape(image)
 #     print(x, y)
 #
 # turtle.onscreenclick(get_mouse_click_coor)
+data = pandas.read_csv('50_states.csv')
+all_states = data['state'].tolist()
+guessed_states = []
 
-state_name = turtle.textinput(title='Guess the State', prompt='What\'s another state\'s name?')
+while len(guessed_states) < 50:
+    state_name = screen.textinput(title=f"{len(guessed_states)}/50 State Correct", prompt='What\'s another state\'s name?').title()
+    if state_name in all_states:
+        new_turtle = turtle.Turtle()
+        new_turtle.hideturtle()
+        new_turtle.penup()
+        state_data = data[data.state == state_name]
+        new_turtle.goto(int(state_data.x), int(state_data.y))
+        new_turtle.write(state_name)
+        guessed_states.append(state_name)
 
-
-print(state_name)
 
 turtle.mainloop()
