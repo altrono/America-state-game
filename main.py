65f4863992f4265a573152ef9bcd3b6cbd13a561
altrono <hbotema16@gmail.com>
2023-01-27 18:18:56 +0200

    Added Image and Textinput on the screen

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor)
 
 state_name = turtle.textinput(title='Guess the State', prompt='What\'s another state\'s name?')
-state_name.penup()
-state_name.goto(-275, 240)
+
+
 print(state_name)
 
 turtle.mainloop()
